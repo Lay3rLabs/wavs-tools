435a2154049bb466b4c531ceaeb9dfb90ae8fe66
Jakub Bogucki <jakub@moonbite.space>
2025-05-26 15:31:48 +0200

    Prepare example solidity interfaces for avs reader based on original eigenlayer avs reader

--- avs_sync/src/interfaces/IOperatorStateRetriever.sol
+++ avs_sync/src/interfaces/IOperatorStateRetriever.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+struct Operator {
+    address operator;
+    uint256 stake;
+}
+
 interface IOperatorStateRetriever {
-    struct Operator {
-        address operator;
-        bytes32 operatorId;
-        uint96 stake;
-    }
+    function GetOperatorState(
+        address registryCoordinator,
+        uint8[] calldata quorumNumbers,
+        uint32 blockNumber
+    ) external view returns (Operator[][] memory);
 
-    function GetOperatorState(address registryCoordinator, uint8[] calldata quorumNumbers, uint32 blockNumber)
-        external
-        view
-        returns (Operator[][] memory);
-
-    function GetOperatorState0(address registryCoordinator, bytes32 operatorId, uint32 blockNumber)
-        external
-        view
-        returns (uint256 quorumBitmap, Operator[][] memory stakes);
+    function GetOperatorState0(
+        address registryCoordinator,
+        bytes32 operatorId,
+        uint32 blockNumber
+    ) external view returns (uint256 quorumBitmap, Operator[][] memory stakes);
 }
--- avs_sync/src/interfaces/IStakeRegistry.sol
+++ avs_sync/src/interfaces/IStakeRegistry.sol
@@ -1,29 +1,7 @@
-// src/interfaces/IStakeRegistry.sol
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 interface IStakeRegistry {
-    struct StakeUpdate {
-        uint32 updateBlockNumber;
-        uint96 stake;
-    }
-
-    function GetCurrentStake(bytes32 operatorId, uint8 quorumNumber) external view returns (uint96);
-    function GetLatestStakeUpdate(bytes32 operatorId, uint8 quorumNumber)
-        external
-        view
-        returns (uint256 blockNumber, uint256 stake);
-
-    /**
-     * @notice Returns the stake of an operator at a specific block number.
-     * @param operatorId ID of the operator.
-     * @param quorumNumber Quorum to query.
-     * @param blockNumber Block number to retrieve the stake from.
-     * @return Stake amount of the operator at the specified block.
-     */
-    function GetStakeAtBlockNumber(bytes32 operatorId, uint8 quorumNumber, uint32 blockNumber)
-        external
-        view
-        returns (uint96);
-
-    function UpdateOperators(address[] calldata operators) external;
+    function GetCurrentStake(bytes32 operatorId, uint8 quorumNumber) external view returns (uint256);
+    function GetLatestStakeUpdate(bytes32 operatorId, uint8 quorumNumber) external view returns (uint256 blockNumber, uint256 stake);
 }
