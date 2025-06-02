01dd7cf35a87aa1cf4924c4d054000c4c4224773
Jakub <jakub@moonbite.space>
2025-06-02 19:19:41 +0200

    Add simple test to pass CI

---
+++ avs_sync/test/AvsReader.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/contracts/AvsReader.sol";
+
+contract AvsReaderTest is Test {
+    AvsReader public avsReader;
+
+    address constant REGISTRY_COORDINATOR = address(0x1);
+    address constant STAKE_REGISTRY = address(0x2);
+    address constant OPERATOR_STATE_RETRIEVER = address(0x3);
+
+    function setUp() public {
+        avsReader = new AvsReader(REGISTRY_COORDINATOR, STAKE_REGISTRY, OPERATOR_STATE_RETRIEVER);
+    }
+
+    function test_Constructor() public view {
+        assertEq(avsReader.registryCoordinator(), REGISTRY_COORDINATOR);
+        assertEq(avsReader.stakeRegistry(), STAKE_REGISTRY);
+        assertEq(avsReader.operatorStateRetriever(), OPERATOR_STATE_RETRIEVER);
+    }
+}
