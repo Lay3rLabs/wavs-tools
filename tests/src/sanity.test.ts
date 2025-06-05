20f6e0aa0d86ff9ccaa032c7c2745f6ac0c99db8
David Komer <6406986+dakom@users.noreply.github.com>
2025-06-05 12:40:25 +0300

    basic mocha setup

---
+++ tests/src/sanity.test.ts
@@ -0,0 +1,10 @@
+import { assert } from "chai";
+
+describe("Placeholder tests", () => {
+  it("should fail", () => {
+    assert.equal(1 + 1, 3);
+  });
+  it("should pass", () => {
+    assert.equal(1 + 1, 2);
+  });
+});
