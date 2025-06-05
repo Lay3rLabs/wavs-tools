fb83770e7e12b4a8916ce105379b25dc75f3b2b9
David Komer <6406986+dakom@users.noreply.github.com>
2025-06-05 13:16:43 +0300

    ci

--- tests/src/sanity.test.ts
+++ tests/src/sanity.test.ts
@@ -15,8 +15,13 @@ describe("WAVS Tools Sanity Tests", () => {
     assert.include(arr, 2);
   });
   
-  // This test can be used to demonstrate failure reporting
+  // This test can be used to demonstrate skipped-test reporting
   it.skip("should demonstrate failure (skipped)", () => {
     assert.equal(2 + 2, 5, "This would fail: 2 + 2 should not equal 5");
+  });
+
+  // This test can be used to demonstrate failure reporting
+  it("should demonstrate failure", () => {
+    assert.equal(2 + 2, 5);
   });
 });
