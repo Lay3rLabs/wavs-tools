8a3db137dcde82ae371cff697a2c88c60bd623d7
ismellike <glopez11193@gmail.com>
2025-06-10 17:04:20 -0500

    Progress on taskfile revamp
    working up to deploy service

--- tools/avs_sync/script/DeployAvsWriter.s.sol
+++ tools/avs_sync/script/DeployAvsWriter.s.sol
@@ -2,31 +2,30 @@
 pragma solidity ^0.8.27;
 
 import {Script} from "forge-std/Script.sol";
-import "forge-std/console.sol";
+import {console} from "forge-std/console.sol";
 import {AvsWriter} from "../src/contracts/AvsWriter.sol";
-import {ECDSAStakeRegistry} from "@eigenlayer-middleware/src/unaudited/ECDSAStakeRegistry.sol";
 import {IWavsServiceManager} from "@wavs/interfaces/IWavsServiceManager.sol";
+import {ECDSAStakeRegistry} from "@eigenlayer-middleware/src/unaudited/ECDSAStakeRegistry.sol";
 
 contract DeployAvsWriter is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PK");
-
-        // Get addresses from environment variables
-        address serviceManager = vm.envAddress("WAVS_SERVICE_MANAGER_ADDRESS");
-        address ecdsaStakeRegistryAddress = vm.envAddress("ECDSA_STAKE_REGISTRY_ADDRESS");
-
-        console.log("=== Deploying AVS Contracts ===");
-        console.log("ECDSA Stake Registry:", ecdsaStakeRegistryAddress);
-        console.log("Service Manager:", serviceManager);
-        console.log("Deployer:", vm.addr(deployerPrivateKey));
-
-        vm.startBroadcast(deployerPrivateKey);
-
-        // Deploy AvsWriter
-        AvsWriter avsWriter =
-            new AvsWriter(IWavsServiceManager(serviceManager), ECDSAStakeRegistry(ecdsaStakeRegistryAddress));
-
-        console.log("AvsWriter deployed at:", address(avsWriter));
+        // Get environment variables
+        address serviceManagerAddress = vm.envAddress("WAVS_SERVICE_MANAGER_ADDRESS");
+        address stakeRegistryAddress = vm.envAddress("ECDSA_STAKE_REGISTRY_ADDRESS");
+        
+        require(serviceManagerAddress != address(0), "WAVS_SERVICE_MANAGER_ADDRESS not set");
+        require(stakeRegistryAddress != address(0), "ECDSA_STAKE_REGISTRY_ADDRESS not set");
+        
+        vm.startBroadcast();
+        
+        // Deploy AvsWriter contract
+        AvsWriter avsWriter = new AvsWriter(
+            IWavsServiceManager(serviceManagerAddress),
+            ECDSAStakeRegistry(stakeRegistryAddress)
+        );
+        
         vm.stopBroadcast();
+        
+        console.log("AvsWriter deployed at:", address(avsWriter));
     }
 }
