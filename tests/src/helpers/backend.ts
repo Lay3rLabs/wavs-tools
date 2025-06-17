bb69ff044dd9c14ec687e9471e69ca3ee2e2a36a
David Komer <6406986+dakom@users.noreply.github.com>
2025-06-17 20:21:54 +0300

    fix after merge and cleanup

--- tests/src/helpers/backend.ts
+++ tests/src/helpers/backend.ts
@@ -68,16 +68,4 @@ export class BackendManager {
   }
 }
 
-function executeCommand(command:string) {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve(stdout);
-    });
-  });
-}
-
 export default BackendManager;
