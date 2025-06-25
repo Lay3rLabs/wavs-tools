41f808a43ec64c142dccea0bc6f7b0f86abebd87
ismellike <glopez11193@gmail.com>
2025-06-25 14:17:37 -0500

    Set up the component to read in the register event

--- projects/multi-chain-operator-sync/component/src/lib.rs
+++ projects/multi-chain-operator-sync/component/src/lib.rs
@@ -1,8 +1,13 @@
 #[allow(warnings)]
 mod bindings;
 
+use alloy_primitives::LogData;
 use alloy_sol_macro::sol;
+use alloy_sol_types::SolEventInterface;
 use bindings::{export, wavs::worker::layer_types::WasmResponse, Guest, TriggerAction};
+use wavs_wasi_utils::decode_event_log_data;
+
+use crate::bindings::wavs::worker::layer_types::TriggerData;
 
 sol!(interface IMirrorUpdateTypes {
     error InvalidTriggerId(uint64 expectedTriggerId);
@@ -17,10 +22,38 @@ sol!(interface IMirrorUpdateTypes {
     }
 });
 
+sol!(
+    #[sol(rpc)]
+    ECDSAStakeRegistry,
+    "../../../abi/eigenlayer-middleware/ECDSAStakeRegistry.sol/ECDSAStakeRegistry.json"
+);
+
 struct Component;
 
 impl Guest for Component {
-    fn run(_action: TriggerAction) -> std::result::Result<Option<WasmResponse>, String> {
+    fn run(action: TriggerAction) -> std::result::Result<Option<WasmResponse>, String> {
+        let evm_trigger_data = match action.data {
+            TriggerData::EvmContractEvent(trigger_data_evm_contract_event) => {
+                Ok(trigger_data_evm_contract_event)
+            }
+            _ => Err(format!(
+                "Only evm contract event triggers are supported. Received {:?}",
+                action
+            )),
+        }?;
+
+        let maybe_register_event: anyhow::Result<ECDSAStakeRegistry::OperatorRegistered> =
+            decode_event_log_data!(evm_trigger_data.log.clone());
+
+        if let Ok(register_event) = maybe_register_event {
+            let _ = register_event;
+        } else {
+            return Err(format!(
+                "Could not decode the event {:?}",
+                evm_trigger_data.log
+            ));
+        }
+
         Ok(None)
     }
 }
