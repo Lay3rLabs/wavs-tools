0ab10384bafad11e288c3c8d1a4a88d15bed70df
ismellike <glopez11193@gmail.com>
2025-07-07 15:55:16 -0500

    Test-case for register operators
    Uses task test:get-operator-weight-updated-event

---
+++ tests/src/multi-chain-operator-sync.test.ts
@@ -0,0 +1,35 @@
+import { BackendManager } from "./helpers/backend";
+import { projectPath, execAsync } from "./helpers/utils";
+
+describe("MULTI-CHAIN-OPERATOR-SYNC", function () {
+  let backendManager: BackendManager;
+
+  before(async function () {
+    // 15 minute timeout for setup
+    // since it includes starting the backend, deploying middleware, registering operators, etc.
+    this.timeout(900000); 
+
+    backendManager = new BackendManager({nChains: 3});
+    await backendManager.start();
+
+    await execAsync("task", ["bootstrap"], {
+      cwd: projectPath("multi-chain-operator-sync"),
+    });
+  });
+
+  after(async function () {
+    this.timeout(30000); // 30 second timeout for cleanup
+    await backendManager.stop();
+  });
+
+  describe("All tests", function () {
+    it("should complete without error", async function () {
+      this.timeout(30000);
+      backendManager.assertRunning();
+
+      await execAsync("task", ["run-tests"], {
+        cwd: projectPath("multi-chain-operator-sync"),
+      });
+    });
+  });
+});
