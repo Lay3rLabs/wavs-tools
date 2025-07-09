db057956e2d3dea1d9aac780ab96090f9bdb9f90
ismellike <glopez11193@gmail.com>
2025-07-09 11:03:46 -0500

    signing key -> signing key address
    point @wavs/solidity to latest git

--- projects/multi-chain-operator-sync/component/src/lib.rs
+++ projects/multi-chain-operator-sync/component/src/lib.rs
@@ -28,7 +28,7 @@ sol!(interface IMirrorUpdateTypes {
         uint64 triggerId;
         uint256 thresholdWeight;
         address[] operators;
-        address[] signingKeys;
+        address[] signingKeyAddresses;
         uint256[] weights;
     }
 });
@@ -115,12 +115,15 @@ async fn handle_register_event(
     );
 
     // Query the current signing key for operator
-    let signing_key = stake_registry
+    let signing_key_address = stake_registry
         .getLatestOperatorSigningKey(operator)
         .call()
         .await?;
 
-    host::log(LogLevel::Info, &format!("Signing key: {}", signing_key));
+    host::log(
+        LogLevel::Info,
+        &format!("Signing key address: {}", signing_key_address),
+    );
 
     // Get operator's stake
     let weight = stake_registry.getOperatorWeight(operator).call().await?;
@@ -140,7 +143,7 @@ async fn handle_register_event(
 
     Ok(UpdateWithId {
         operators: vec![operator],
-        signingKeys: vec![signing_key],
+        signingKeyAddresses: vec![signing_key_address],
         weights: vec![weight],
         triggerId: block_height,
         thresholdWeight: threshold_weight,
@@ -175,7 +178,7 @@ async fn handle_deregister_event(
         triggerId: block_height,
         thresholdWeight: threshold_weight,
         operators: vec![operator],
-        signingKeys: vec![Address::ZERO],
+        signingKeyAddresses: vec![Address::ZERO],
         weights: vec![Uint::ZERO],
     })
 }
