283615349a8ceb74b63c7ef8a44cf7a8c37862ad
Reece Williams <reecepbcups@gmail.com>
2025-09-09 18:44:34 -0500

    wip idk what I am doing

--- projects/aggregator-simple/component/src/lib.rs
+++ projects/aggregator-simple/component/src/lib.rs
@@ -2,6 +2,10 @@
 #[rustfmt::skip]
 mod bindings;
 
+use alloy_primitives::{TxHash, hex};
+use alloy_provider::{Provider, RootProvider};
+use alloy_network::Ethereum;
+
 use crate::bindings::{
     export, host,
     wavs::{
@@ -10,6 +14,8 @@ use crate::bindings::{
     },
     AggregatorAction, AnyTxHash, Guest, Packet,
 };
+use wavs_wasi_utils::evm::{alloy_primitives::Address, new_evm_provider};
+use wstd::runtime::block_on;
 
 struct Component;
 
@@ -60,11 +66,66 @@ impl Guest for Component {
         _packet: Packet,
         tx_result: Result<AnyTxHash, String>,
     ) -> Result<(), String> {
-        match tx_result {
-            Ok(_) => Ok(()),
-            Err(_) => Ok(()),
+        // TODO: add some configurable delay here (a few blocks for ethereum)
+        let res = tx_result.clone().unwrap();
+        println!("Submit callback called with tx hash: {:?}", res);
+
+        let evm = match res {
+            AnyTxHash::Evm(evm) => evm,
+            AnyTxHash::Cosmos(_) => return Err("Expected EVM tx hash, got Cosmos".to_string()),
+        };
+
+        let tx_hash: TxHash = TxHash::from_slice(&evm);
+
+        // query the tx_hash from the evm node via the host
+
+        let service = host::get_service();
+        let chain_name = match service.service.manager {
+            bindings::wavs::types::service::ServiceManager::Evm(evm_manager) => {
+                evm_manager.chain_name
+            }
+        };
+
+        let receipt_result = block_on(async move {
+            let provider = create_provider(&chain_name).await.unwrap();
+
+            // TODO: if the receipt does not exist after our delay then we can assume the tx has been reverted
+            let receipt = provider
+                .get_transaction_receipt(tx_hash)
+                .await
+                .map_err(|e| format!("Failed to get transaction receipt: {}", e))?
+                .ok_or("Transaction receipt not found".to_string())?;
+
+
+            println!("Transaction receipt: {:?}", receipt);
+
+
+            Ok::<_, String>(receipt)
+        });
+
+        match receipt_result {
+            Ok(_receipt) => match tx_result {
+                Ok(_) => Ok(()),
+                Err(e) => {
+                    println!("Transaction query failed (must have been re-orged): {}", e);
+                    Ok(())
+                },
+            },
+            Err(e) => Err(e),
         }
     }
 }
 
 export!(Component with_types_in bindings);
+
+/// Creates a provider instance for EVM queries
+async fn create_provider(chain_name: &str) -> Result<RootProvider<Ethereum>, String> {
+    let chain_config = host::get_evm_chain_config(chain_name)
+        .ok_or(format!("Failed to get chain config for {}", chain_name))?;
+
+    let provider = new_evm_provider::<Ethereum>(
+        chain_config.http_endpoint.ok_or("No HTTP endpoint configured")?,
+    );
+
+    Ok(provider)
+}
